ff67332a6bc027d3157b6f9ea24e4ba380eb541e
Divya S <divya0517dd@gmail.com>
2026-08-27 14:10:40 +0530

    Complete MongoDB CRUD integration

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,25 +5,17 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true,
     },
-
     email: {
       type: String,
       required: true,
-      trim: true,
-      unique: true,
     },
-
     phone: {
       type: String,
       required: true,
-      trim: true,
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
